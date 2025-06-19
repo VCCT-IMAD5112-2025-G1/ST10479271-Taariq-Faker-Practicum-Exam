29f5b2073d958a8b6bc5d1af5ca25f7deebe7b53
ST10479271 <ST10479271@vcconnect.edu.za>
2025-06-19 09:21:58 +0200

    Created The pages and code the exit and next button

--- app/src/main/java/vcmsa/ci/st10479271taariqfakerpracticumexam/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/st10479271taariqfakerpracticumexam/MainActivity.kt
@@ -2,8 +2,12 @@ package vcmsa.ci.st10479271taariqfakerpracticumexam
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,14 +15,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
 
-    var song= arrayOfNulls<String>(10)
+    var songArray = ArrayList<String>()
 
-    var artist= arrayOfNulls<String>(10)
+    var artistArray = ArrayList<String>()
 
-    var rating= arrayOfNulls<Int>(10)
+    var ratingArray = ArrayList<Int>()
 
-    var comments= arrayOfNulls<String>(10)
-
+    var commentsArray = ArrayList<String>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val ExitButton=findViewById<Button>(R.id.btnExit)
-        val NextPage=findViewById<Button>(R.id.btnNext)
-        val AddToPlaylist=findViewById<Button>(R.id.btnAdd)
+        val ExitButton = findViewById<Button>(R.id.btnExit)
+        val NextPage = findViewById<Button>(R.id.btnNext)
+        val AddToPlaylist = findViewById<Button>(R.id.btnAdd)
 
 
         ExitButton.setOnClickListener {
@@ -37,22 +40,58 @@ class MainActivity : AppCompatActivity() {
 
         NextPage.setOnClickListener {
 
-            val intent= Intent(this,DetailedViewScreen::class.java)
+            val intent = Intent(this, DetailedViewScreen::class.java)
 
 
         }
 
         AddToPlaylist.setOnClickListener {
 
-            song[0]="Songs"
+            AddSongs()
 
 
         }
 
-
-        fun Songs (){}
-        println("Please Enter a song")
-        val songName= readln()
-
-        }
     }
+
+    private fun AddSongs() {}
+    val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_add_songs_list, null)
+
+
+    val SongInput = dialogView.findViewById<EditText>(R.id.EditTextSong)
+    val ArtistName = dialogView.findViewById<EditText>(R.id.editTextArtist)
+    val RatingInput = dialogView.findViewById<EditText>(R.id.editTextRating)
+    val CommentsInput = dialogView.findViewById<EditText>(R.id.editTextComments)
+
+    val dialog = AlertDialog.Builder(this)
+        .setTitle("Add Song Name")
+        .setView(dialogView)
+        .setPositiveButton("Add")
+        { _, _ ->
+
+            val song = SongInput.text.toString()
+            val artist = ArtistName.text.toString()
+            val Rating = RatingInput.text.toString().toIntOrNull()
+            val Comments = CommentsInput.text.toString()
+
+            if (song.isNotEmpty() && artist.isNotEmpty() && Rating != null && Rating in 1..5) {
+                songArray.add(song)
+                artistArray.add(artist)
+                ratingArray.add(Rating)
+                commentsArray.add(Comments)
+
+                Toast.makeText(this, "Song Added", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please fill all the field correctly", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        .setNegativeButton("Cancel", null)
+        .create()
+
+    dialog.show()
+
+
+}
+
+
