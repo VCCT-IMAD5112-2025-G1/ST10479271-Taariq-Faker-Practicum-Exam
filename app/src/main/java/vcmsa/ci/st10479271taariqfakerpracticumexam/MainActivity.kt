b5e0b7f20e98c6e29b243e2ad9eadd1f10678798
ST10479271 <ST10479271@vcconnect.edu.za>
2025-06-19 08:46:00 +0200

    Created The pages and code the exit and next button

--- app/src/main/java/vcmsa/ci/st10479271taariqfakerpracticumexam/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/st10479271taariqfakerpracticumexam/MainActivity.kt
@@ -1,20 +1,58 @@
 package vcmsa.ci.st10479271taariqfakerpracticumexam
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    var song= arrayOfNulls<String>(10)
+
+    var artist= arrayOfNulls<String>(10)
+
+    var rating= arrayOfNulls<Int>(10)
+
+    var comments= arrayOfNulls<String>(10)
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val ExitButton=findViewById<Button>(R.id.btnExit)
+        val NextPage=findViewById<Button>(R.id.btnNext)
+        val AddToPlaylist=findViewById<Button>(R.id.btnAdd)
+
+
+        ExitButton.setOnClickListener {
+            finishAffinity()
+        }
+
+        NextPage.setOnClickListener {
+
+            val intent= Intent(this,DetailedViewScreen::class.java)
+
+
+        }
+
+        AddToPlaylist.setOnClickListener {
+
+            song[0]="Songs"
+
+
+        }
+
+
+        fun Songs (){}
+        println("Please Enter a song")
+        val songName= readln()
+
         }
     }
-}
